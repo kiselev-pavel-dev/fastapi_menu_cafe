9028eb5752e9f67e94464e18e6966b2c8dcac002
Павел Киселев <neznika2@yandex.ru>
2023-01-24 17:41:42 +0300

    Fix bugs

--- tests/conf_test_db.py
+++ tests/conf_test_db.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from src.settings import settings
-from src.database import Base, get_db
+from src.database import get_db
 from src.main import app
 
 POSTGRES_USER = settings.POSTGRES_USER
 POSTGRES_PASSWORD = settings.POSTGRES_PASSWORD
 DB_HOST = settings.DB_HOST
-DB_NAME = settings.POSTGRES_DB_TESTS
+DB_NAME = settings.POSTGRES_DB
 DB_PORT = settings.DB_PORT
 
 if not settings.docker_mode:
@@ -21,7 +21,9 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.drop_all(bind=engine)
+Base = declarative_base()
+
+# Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 
