b5d62c50943335d069768a54a381a60f0ef8856b
Павел Киселев <neznika2@yandex.ru>
2023-01-25 16:06:39 +0300

    Fix bugs

--- tests/conf_test_db.py
+++ tests/conf_test_db.py
@@ -9,7 +9,7 @@ from src.tables import Base
 
 POSTGRES_USER = settings.POSTGRES_USER
 POSTGRES_PASSWORD = settings.POSTGRES_PASSWORD
-DB_HOST = settings.DB_HOST
+DB_HOST = "db_tests"
 DB_NAME = settings.POSTGRES_DB_TESTS
 DB_PORT = settings.DB_PORT
 
